f0c038bd0f11665477e72ef88b76eff66d3bc688
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-26 19:07:36 +0200

    Add Book table migration

---
+++ app/database/migrations/2014_09_26_170328_create_books_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('knjige', function(Blueprint $table)
+		{
+			$table->string('Naslov');
+			$table->string('Autor');
+			$table->string('Impresum');
+			$table->string('Materijalni');
+			$table->string('NakladnaCjelina');
+			$table->string('Napomena');
+			$table->string('Naklada');
+			$table->string('Sadrzaj');
+			$table->string('ISBN');
+			$table->string('Signatura');
+			$table->integer('CIP');
+			$table->string('Izdanje');
+			$table->integer('Otpis');
+			$table->string('Tip');
+			$table->string('Nabava');
+			$table->string('Kupljeno');
+			$table->increments('id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('knjige');
+	}
+
+}
