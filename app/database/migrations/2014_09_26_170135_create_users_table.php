e72556e02a7c9d6ec09a5d7406bc282a3f129987
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-26 19:03:42 +0200

    Create User table migration

---
+++ app/database/migrations/2014_09_26_170135_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('korisnici', function(Blueprint $table)
+		{
+			$table->string('Ime');
+			$table->string('Prezime');
+			$table->string('Tip');
+			$table->integer('Razred');
+			$table->increments('id');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('korisnici');
+	}
+
+}
