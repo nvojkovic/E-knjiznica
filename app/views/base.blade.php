5edb24bd4ca51956edfc18e4e2587513f564182f
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-27 20:46:08 +0200

    Remove dummy data for morris

--- app/views/base.blade.php
+++ app/views/base.blade.php
@@ -105,7 +105,6 @@
     <!-- Morris Charts JavaScript -->
     <script src="{{ URL::asset('js/plugins/morris/raphael.min.js')}}"></script>
     <script src="{{ URL::asset('js/plugins/morris/morris.min.js')}}"></script>
-    <script src="{{ URL::asset('js/plugins/morris/morris-data.js')}}"></script>
 
 </body>
 
