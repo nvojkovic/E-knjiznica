925cf68d47548cb55784ba47171cb41b43b626cf
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-27 22:56:03 +0200

    Add AV table migration

---
+++ app/database/migrations/2014_09_27_205349_create_av_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('AV', function(Blueprint $table)
+		{
+			$table->string('Naslov');
+			$table->string('Autor');
+			$table->string('Izdavac');
+			$table->string('Napomena');
+			$table->increments('ID');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('AV');
+	}
+
+}
