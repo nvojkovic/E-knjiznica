3035628049eaa5da8770c069fa3bc05344aa65e0
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-27 20:46:28 +0200

    Add some routes

--- app/routes.php
+++ app/routes.php
@@ -12,5 +12,19 @@
 */
 
 Route::get('/', 'DashboardController@Home');
+
+//API routes
 Route::get('/api/korisnik/{id}', 'ApiController@User');
-Route::get('/api/knjiga/:id', 'ApiController@Book');
+Route::get('/api/knjiga/{id}', 'ApiController@Book');
+
+//Book routes
+Route::get('/knjiga/dodaj', 'BookController@Add');
+Route::post('/knjiga/dodaj', 'BookController@AddPost');
+Route::get('/knjiga/posudi', 'BookController@Borrow');
+Route::post('/knjiga/posudi', 'BookController@BorrowPost');
+Route::get('/knjiga/vrati', 'BookController@Returning');
+Route::post('/knjiga/vrati', 'BookController@ReturningPost');
+
+//User routes
+Route::get('/ucenik/dodaj', 'UserController@Add');
+Route::post('/ucenik/dodaj', 'UserController@AddPost');
