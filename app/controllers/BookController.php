fb2a5d14bd0ee43e89a829f059f5bee9ce5d26cf
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-27 20:44:10 +0200

    Add borrow code to book controller

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -15,4 +15,25 @@ class BookController extends BaseController {
 		}	
 		return View::Make('addBook', array('message' => 'Knjiga uspješno dodana! :)'));
 	}
+
+	public function Borrow()
+	{
+		return View::Make('borrow');
+	}
+
+	public function BorrowPost()
+	{
+		$user =User::find(Input::get('user'));
+		$book =Book::find(Input::get('book'));
+
+		$borrow = new Borrow;
+		$borrow->Korisnik = Input::get('user');
+		$borrow->Knjiga = Input::get('book');
+		$borrow->DatumPosudbe = date("Y-m-d h:i:s");
+		$borrow->save();
+
+		return View::Make('borrow', array('user' => $user->Ime." ".$user->Prezime, 'book' => $book->Naslov));
+	}
+
+
 }
