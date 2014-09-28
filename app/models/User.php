749238c8bf18e1a1dbb30d0f36e833d8eab44cda
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-28 13:33:32 +0200

    Add Grade relationship to User model

--- app/models/User.php
+++ app/models/User.php
@@ -5,4 +5,9 @@ class User extends Eloquent{
 	public $timestamps = false;	
 	protected $table = 'korisnici';
 	protected $fillable = array('*');
+
+	public function grade()
+	{
+		return $this->hasOne('Grade', 'Ucenik', 'Razred');
+	}
 }
