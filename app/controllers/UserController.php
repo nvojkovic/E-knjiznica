5897db6db23e1faa2b4af9d86d5a7cb4d7982846
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-09-30 23:29:06 +0200

    Add user history methods

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -14,4 +14,31 @@ class UserController extends BaseController {
 		$grades = Grade::all();
 		return View::Make('addUser', array('message' => 'Korisnik uspješno dodan! :)', 'grades' => $grades));
 	}
+
+	public function History()
+	{
+		return View::Make('userHistory');
+	}
+
+	public function HistoryPost()
+	{
+		$user = User::find(Input::get('user'));
+		$grade = $user->grade;
+		$history2 = $user->borrow;
+		$history = array();
+		foreach($history2 as $item)
+		{
+			$temp = array();
+			$book = $item->book;
+			$temp['Naslov'] = $book->Naslov;
+			$temp['Autor'] = $book->Autor;
+			$temp['DatumPosudbe'] = date('d.m.Y', strtotime($item->DatumPosudbe));
+			if(strtotime($item->DatumVracanja) == 0)
+				$temp['DatumVracanja'] = "";
+			else
+				$temp['DatumVracanja'] = date('d.m.Y', strtotime($item->DatumVracanja));
+			array_push($history, $temp);
+		}
+		return View::Make('userHistory', array('user' => $user, 'grade' => $grade, 'history' => $history));
+	}
 }
