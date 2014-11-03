54495c275db0481391199ec559941cc0d1ff5fe0
Nikola Vojkovic <vojkovic.nikola@xnet.hr>
2014-11-03 14:55:26 +0100

    Fix Sticker printing for books

--- app/controllers/StickerController.php
+++ app/controllers/StickerController.php
@@ -4,13 +4,14 @@ class StickerController extends BaseController {
 
 	public function Create()
 	{
-		return View::make('printStickers');
+		$books = Book::orderBy('BookID', 'DESC')->take(100)->get();
+		return View::make('printStickers', array('books' => $books));
 	}
 
 	public function CreatePost()
 	{
 
-		header('content-type: application/pdf');
+		//header('content-type: application/pdf');
 
 		//create pdf, set landscape, paper size, margins, font
 		$pdf = new FPDF('L','cm','A4');
@@ -32,13 +33,38 @@ class StickerController extends BaseController {
 		//$i is offset for first page (i-1 bug)
 		$i = Input::get('offset')-1;
 		//ids are all users whose cards we are printing
-		$ids = Input::get('print');
+		$ids = Input::get('IDs');
+
+		$ids2 = str_replace(" ", "", $ids);
+		$ids2 = explode(",", $ids2);
+
+		$ids3 = array();
+
+		foreach ($ids2 as $id)
+		{
+			if(!strpos($id, '-'))
+			{
+				$ids3[] = $id;
+			}
+			else
+			{
+				$start = explode("-", $id)[0];
+				$end = explode("-", $id)[1];
+				for($i = $start; $i <= $end; $i++)
+				{
+					$ids3[] = $i;
+				}
+			}
+		}
+
+		$ids = $ids3;
+		//print_r($ids3);
 
 		if($i != 0)
 		{
 			$pdf->AddPage();
 		}
-		foreach ($ids as $id)
+		foreach ($ids3 as $id)
 		{
 			
 			$book = Book::find($id);
@@ -66,7 +92,7 @@ class StickerController extends BaseController {
 			imagecopymerge($image, $barcode, $dest_x, $dest_y, 0, 0, $barcode_width, $barcode_height,100);
 
 			//write data under barcode (for manual input)
-			ImageTTFText($image,15,270,115,210, $black,$font, "K".$book->ID);
+			ImageTTFText($image,15,270,115,210, $black,$font, "K".$book->BookID);
 
 			//write data to image
 			ImageTTFText($image,40,0,10,75, $black,$font, "2");
@@ -74,59 +100,59 @@ class StickerController extends BaseController {
 			ImageTTFText($image,40,0,10,210, $black,$font, "b");
 			
 			//destroy stuff
-			imagejpeg($image, 'K'.$book->ID.".jpg");
+			imagejpeg($image, 'K'.$book->BookID.".jpg");
 			imagedestroy($image);  
 			imagedestroy($barcode);
 			
 			if($i % 21 == 0)
 			{
 				$pdf->AddPage();
-				$pdf->Image('K'.$book->ID.".jpg", 0, 0, $sizex,$sizey);	
+				$pdf->Image('K'.$book->BookID.".jpg", 0, 0, $sizex,$sizey);	
 			}
 			if($i % 21 == 1)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + $offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + $offsetx, 0, $sizex,$sizey);
 			if($i % 21 == 2)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 2*$offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 2*$offsetx, 0, $sizex,$sizey);
 			if($i % 21 == 3)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 3*$offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 3*$offsetx, 0, $sizex,$sizey);
 			if($i % 21 == 4)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 4*$offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 4*$offsetx, 0, $sizex,$sizey);
 			if($i % 21 == 5)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 5*$offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 5*$offsetx, 0, $sizex,$sizey);
 			if($i % 21 == 6)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 6*$offsetx, 0, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 6*$offsetx, 0, $sizex,$sizey);
 
 
 			if($i % 21 == 7)
-				$pdf->Image('K'.$book->ID.".jpg", 0, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 8)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + $offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + $offsetx, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 9)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 2*$offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 2*$offsetx, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 10)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 3*$offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 3*$offsetx, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 11)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 4*$offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 4*$offsetx, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 12)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 5*$offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 5*$offsetx, 0 + $offsety, $sizex,$sizey);
 			if($i % 21 == 13)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 6*$offsetx, 0 + $offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 6*$offsetx, 0 + $offsety, $sizex,$sizey);
 			
 
 			if($i % 21 == 14)
-				$pdf->Image('K'.$book->ID.".jpg", 0, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 15)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + $offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + $offsetx, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 16)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 2*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 2*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 17)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 3*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 3*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 18)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 4*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 4*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 19)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 5*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 5*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
 			if($i % 21 == 20)
-				$pdf->Image('K'.$book->ID.".jpg", 0 + 6*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
+				$pdf->Image('K'.$book->BookID.".jpg", 0 + 6*$offsetx, 0 + 2*$offsety, $sizex,$sizey);
 
 			$i++;
 
